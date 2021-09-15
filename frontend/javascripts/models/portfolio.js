02021037dce991ebffb6f9fe897cba649b47498a
Gia Jennings <gjennings.co@gmail.com>
2021-09-15 13:28:03 -0400

    Define static methods for POrtfolio class

--- frontend/javascripts/models/portfolio.js
+++ frontend/javascripts/models/portfolio.js
@@ -9,4 +9,20 @@ class Portfolio {
         this.stocks = stocks;
         Portfolio.all.push(this)
     }
+
+    static getAll() {
+        return this.all
+    }
+
+    static findByName(investor) {
+       return this.all.find(function(portfolio) { portfolio.investor === investor})
+    }
+
+    static findById(portfolio_id) {
+        return this.all.find(portfolio => portfolio.portfolio_id === portfolio_id)
+    }
+
+    static findOrCreateBy(pObj) {
+        return this.findByName(pObj.investor) || new Portfolio(pObj)
+    }
 }
